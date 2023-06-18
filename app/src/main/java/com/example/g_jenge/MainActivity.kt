5ae899ff07e4b4153334ef5a99ab3470c6b6d078
devopsbilli <devopsbilli@gmail.com>
2023-06-18 20:31:13 +0300

    Initial Project Setup

--- app/src/main/java/com/example/g_jenge/MainActivity.kt
+++ app/src/main/java/com/example/g_jenge/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.g_jenge
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -10,9 +8,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
