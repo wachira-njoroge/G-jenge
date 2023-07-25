78f6555ef69aa36dd62c11bc2601745db3e934f5
devopsbilli <devopsbilli@gmail.com>
2023-07-25 22:25:07 +0300

    Created the tasks entity that will save all project tasks and added indices that enable da querying

---
+++ app/src/main/java/com/awesome/g_jenge/entities/Tasks.kt
@@ -0,0 +1,37 @@
+package com.awesome.g_jenge.entities
+
+import androidx.annotation.NonNull
+import androidx.room.*
+import androidx.room.ForeignKey.CASCADE
+
+@Entity(tableName = "tasks",
+    foreignKeys =
+        [ForeignKey(entity = Projects::class, parentColumns = ["id"], childColumns = ["projectId"], onDelete = CASCADE)],
+    indices = [Index(value = ["code","projectId"])]
+)
+data class Tasks(
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    val id: Int,
+
+    @ColumnInfo(name="code", index = true)
+    val code: String,
+
+    @ColumnInfo(name="description")
+    val description: String,
+
+    @ColumnInfo(name="status")
+    val status: String,
+
+    @ColumnInfo(name = "due_date")
+    val due_date:String,
+
+    @ColumnInfo(name = "due_time")
+    val due_time: String,
+
+    @ColumnInfo(name = "notified")
+    val notified: String,
+
+    @ColumnInfo(name = "projectId", index = true)
+    val projectId: Int
+)
