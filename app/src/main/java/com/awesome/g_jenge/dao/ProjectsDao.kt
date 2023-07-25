3ed2e358f0a7f9b5cf681eb58d31f439dcc51ab5
devopsbilli <devopsbilli@gmail.com>
2023-07-25 22:21:09 +0300

    Created the projects data access object that contains the various queries that fetch database data

---
+++ app/src/main/java/com/awesome/g_jenge/dao/ProjectsDao.kt
@@ -0,0 +1,22 @@
+package com.awesome.g_jenge.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.awesome.g_jenge.entities.Projects
+import kotlinx.coroutines.flow.Flow
+@Dao
+interface ProjectsDao {
+    @Query("select * from projects order by name asc")
+    fun getAllProjects(): Flow<List<Projects>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(project:Projects)
+
+    @Query("select * from projects where `projects`.name =:projectName")
+    fun getProjectByName(projectName:String): Flow<Projects>
+
+    @Query("DELETE FROM projects where `projects`.code = :code")
+    suspend fun deleteProject(code: String):Void
+}
